943fd368cd4c1a7eb0a99346b5264b29d4640789
mehmet <mehmetkicirti@hotmail.com>
2019-10-28 20:51:25 +0300

    Configurated to movie into app.js.

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const movieRouter=require('./routes/movie');
 const app = express();
+
+//db connection
+const db= require(path.join(__dirname,"/helper","/db.js"))();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/api/movie',movieRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) =>{
   next(createError(404));
