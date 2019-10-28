ed8b514bc2443de9ff76f723712cad9e834fe4fc
mehmet <mehmetkicirti@hotmail.com>
2019-10-29 00:29:32 +0300

    Applied by ıd api method.

--- routes/director.js
+++ routes/director.js
@@ -1,63 +1,113 @@
+const mongoose = require('mongoose'); 
 const express = require('express');
 const router = express.Router();
 
 
 //Models Director
-const Director=require('../models/Director');
+const Director = require('../models/Director');
 
 /* Post. */
-router.post('/', (req, res, next)=> {
+router.post('/', (req, res, next) => {
   const director = new Director(req.body);
   const promise = director.save();
 
-  promise.then((data)=>{
+  promise.then((data) => {
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   });
 });
 //Get all directors 
-router.get('/',(req,res)=>{
-  const promise = Director.aggregate([
-    {
-      $lookup:{
-        from:'movies', //which with table
-        localField:'_id',
-        foreignField:'director_id',
-        as:'movies' //what as assigned name
+router.get('/', (req, res) => {
+  const promise = Director.aggregate([{
+      $lookup: {
+        from: 'movies', //which with table
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies' //what as assigned name
       }
     },
     {
-      $unwind:{
-        path:'$movies',
-        preserveNullAndEmptyArrays:true //Whatever matched any data is getting this command.
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true //Whatever matched any data is getting this command.
       }
     },
     {
-      $group:{
-        _id:{
-          _id:'$_id',
-          name:'$name',
-          surname:'$surname',
-          bio:'$bio'
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
         },
-        movies:{
-          $push:'$movies'
+        movies: {
+          $push: '$movies'
         } // this command was provide that getting into movies all movie belonging the director.
       }
     },
     {
-      $project:{
-        _id:'$_id._id',
-        name:'$_id.name',
-        surname:'$_id.surname',
-        movies:'$_id.movies'
-      }//this command was provided that merging what we wanted properties defining in here. 
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$_id.movies'
+      } //this command was provided that merging what we wanted properties defining in here. 
     }
   ]);
-  promise.then((data)=>{
+  promise.then((data) => {
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+//Get By Id
+router.get('/:director_id', (req, res) => {
+  const promise = Director.aggregate([
+    {
+      $match: {
+        _id: mongoose.Types.ObjectId(req.params.director_id)
+      }
+    },
+    {
+      $lookup: {
+        from: 'movies', //which with table
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies' //what as assigned name
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true //Whatever matched any data is getting this command.
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies: {
+          $push: '$movies'
+        } // this command was provide that getting into movies all movie belonging the director.
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$_id.movies'
+      } //this command was provided that merging what we wanted properties defining in here. 
+    }
+  ]);
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
     res.json(err);
   });
 });
