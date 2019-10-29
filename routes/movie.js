b607587eb08c77a0b541427453dcfd589410b44c
mehmet <mehmetkicirti@hotmail.com>
2019-10-30 00:00:02 +0300

    Movie all api methods has been finished.

--- routes/movie.js
+++ routes/movie.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+const fs = require('fs'); 
 // Error Messages
-let ErrorEnums={
-    Not_Found:1,
-    Not_Found_To_Deleted:2,
-    Not_Found_To_Updated:3,
-    properties:{
-        1:{name:"Not Found",value:"The movie was not found.",code:404},
-        2:{name:"Not Found Deleted",value:"The movie was not found to deleted.",code:405},
-        3:{name:"Not Found Updated",value:"The movie was not found to updated",code:406}
+let ErrorEnums = {
+    Not_Found: 1,
+    Not_Found_To_Deleted: 2,
+    Not_Found_To_Updated: 3,
+    properties: {
+        1: {
+            name: "Not Found",
+            value: "The movie was not found.",
+            code: 404
+        },
+        2: {
+            name: "Not Found Deleted",
+            value: "The movie was not found to deleted.",
+            code: 405
+        },
+        3: {
+            name: "Not Found Updated",
+            value: "The movie was not found to updated",
+            code: 406
+        }
     }
 };
 
@@ -22,113 +34,186 @@ router.get('/', (req, res) => {
     //         res.json(err);
     //     res.json(data);
     // });
-    const promise = Movie.aggregate([
-        {
-            $lookup:{
-                from:'directors',
-                localField:'director_id',
-                foreignField:'_id',
-                as:'director'
-            }
-            
-        },
-        {
-            $lookup:{
-                from:'categories',
-                localField:'category_id',
-                foreignField:'_id',
-                as:'category'
+    const promise = Movie.aggregate([{
+            $lookup: {
+                from: 'directors',
+                localField: 'director_id',
+                foreignField: '_id',
+                as: 'director'
             }
         },
         {
-            $unwind:'$director'
+            $lookup: {
+                from: 'categories',
+                localField: 'category_id',
+                foreignField: '_id',
+                as: 'category'
+            }
         },
         {
-            $unwind:'$category'
+            $lookup: {
+                from: "users",
+                localField: "comments.user_id",
+                foreignField: "_id",
+                as: "user"
+            }
+        },
+        {
+            $unwind: {
+                path:"$director",
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $unwind:{ path:'$category',
+            preserveNullAndEmptyArrays: true}
+        },
+        {
+            $unwind: {path:"$user",
+            preserveNullAndEmptyArrays: true}
+        },
+        {
+            $project:{
+                user:"$user",
+                category:"$category",
+                director:"$director",
+                title:1,
+                imdb_score:1,
+                country:1,
+                imgURL:1,
+                year:1,
+                createdAt:1
+            }
         }
     ]);
-    promise.then((data)=>{
+    promise.then((data) => {
         res.json(data);
-    }).catch((err)=>{
+    }).catch((err) => {
         res.json(err);
-    })
+    });
 });
 //Top 10
-router.get('/top10',(req,res)=>{
-    const promise = Movie.find({}).limit(10).sort({imdb_score:-1});
-    promise.then((data)=>{
+router.get('/top10', (req, res) => {
+    const promise = Movie.find({}).limit(10).sort({
+        imdb_score: -1
+    });
+    promise.then((data) => {
         res.json(data);
-    }).catch((err)=>{
+    }).catch((err) => {
         res.json(err);
     });
 });
 //between year api process
-router.get('/between/:start_year/:end_year',(req,res)=>{
-    const {start,end}=req.params;
-    const promise = Movie.find(
-        {
-            year:{
-                "$gte":parseInt(start),
-                "$lte":parseInt(end)
-            }
+router.get('/between/:start_year/:end_year', (req, res) => {
+    const {
+        start,
+        end
+    } = req.params;
+    const promise = Movie.find({
+        year: {
+            "$gte": parseInt(start),
+            "$lte": parseInt(end)
         }
-    );
+    });
 
-    promise.then((movies)=>{
+    promise.then((movies) => {
         res.json(movies);
-    }).catch((err)=>{
+    }).catch((err) => {
         res.json(err);
     });
 });
 //search by name
-router.get('/searchByTitle/:title',(req,res,next)=>{
-    const promise = Movie.findOne({title:req.params.title});
+router.get('/searchByTitle/:title', (req, res, next) => {
+    const promise = Movie.findOne({
+        title: req.params.title
+    });
 
-    promise.then((movie)=>{
-        if(!movie)
-            next({message:ErrorEnums.properties[0].value,code:ErrorEnums.properties[0].code});
+    promise.then((movie) => {
+        if (!movie)
+            next({
+                message: ErrorEnums.properties[0].value,
+                code: ErrorEnums.properties[0].code
+            });
         res.json(movie);
-    }).catch((err)=>{
+    }).catch((err) => {
         res.json(err);
     });
 });
 
-
-router.get('/:movie_id', (req, res,next) => {
+router.get('/:movie_id', (req, res, next) => {
     //what will be movie_id that to be appointed.
-    Movie.findById(req.params.movie_id,(err,data)=>{
-        if(!data)
-            next({message:ErrorEnums.properties[0].value,code:ErrorEnums.properties[0].code});
-        if(err)
+    Movie.findById(req.params.movie_id, (err, data) => {
+        if (!data)
+            next({
+                message: ErrorEnums.properties[0].value,
+                code: ErrorEnums.properties[0].code
+            });
+        if (err)
             res.json(err);
         res.json(data);
     });
 });
 
-router.put('/:movie_id',(req,res,next)=>{
-    const promise =Movie.findByIdAndUpdate(req.params.movie_id,req.body,{
-        new:true
+router.put('/:movie_id', (req, res, next) => {
+    const {title,
+        imdb_score,
+        country,
+        year} =req.body;
+    const {imgURL}=req.file;
+
+    const movie = new Movie({
+        title:title,
+        imdb_score:imdb_score,
+        country:country,
+        year:year
     });
-    promise.then((movie)=>{
-        if(!movie){
-            next({message:ErrorEnums.properties[1].value,code:ErrorEnums.properties[1].code});
+    if(imgURL){
+        fs.unlink('public/images/'+movie.imgURL, err=>{
+            if(err){
+                console.log(err);
+            }
+        });
+        movie.imgURL = imgURL.filename;
+    }
+    const promise = Movie.findByIdAndUpdate(req.params.movie_id, {
+        movie
+    }, {
+        new: true
+    });
+    promise.then((movie) => {
+        if (!movie) {
+            next({
+                message: ErrorEnums.properties[1].value,
+                code: ErrorEnums.properties[1].code
+            });
         }
         res.json(movie);
-    }).catch((err)=>{
+    }).catch((err) => {
         res.json(err);
     });
 });
 
-router.delete('/:movie_id',(req,res,next)=>{
-    const promise = Movie.findByIdAndRemove(req.params.movie_id);
-    promise.then((movie)=>{
-        if(!movie)
-           next({message:ErrorEnums.properties[2].value,code:ErrorEnums.properties[2].code}); 
-
-        res.json(movie);
-    }).catch((err)=>{
-        res.json(err);
-    });
+router.delete('/:movie_id', (req, res, next) => {
+    
+    Movie.findOne({_id:req.params.movie_id}).
+        then((movie)=>{
+            if(!movie){
+             next({message:"Movie was not found.",status:404});
+            }
+            fs.unlink('public/images/'+movie.imgURL, err=>{
+                    if(err){
+                        console.log(err);
+                    }
+            });
+            movie.remove((err,movie)=>{
+                if(err)
+                    res.json(err);
+                res.json({
+                    message:"Successfully Deleted.",
+                    movie:movie
+                });
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
@@ -136,13 +221,22 @@ router.post('/', (req, res, next) => {
         title,
         imdb_score,
         country,
-        year
+        year,
+        comments,
+        category_id,
+        director_id
     } = req.body; //all objects contain to post
+    const {imgURL}=req.file;
+
     const movie = new Movie({
         title: title,
         imdb_score: imdb_score,
         country: country,
-        year: year
+        year: year,
+        comments: comments,
+        category_id,
+        director_id,
+        imgURL:imgURL.filename
     });
     // movie.save((err,data)=>{
     //     if(err)
@@ -156,6 +250,30 @@ router.post('/', (req, res, next) => {
     });
 });
 
+router.put('/addComment/:movie_id', (req, res, next) => {
+    const {
+        user_id,
+        message
+    } = req.body;
+    Movie.findByIdAndUpdate({
+        _id: req.params.movie_id
+    }, {
+        $push: {
+            comments: [{
+                user_id,
+                message
+            }]
+        }
+    }, {
+        new: true,
+        safe: true,
+        upsert: true
+    }, (err, movie) => {
+        if (err)
+            res.json(err);
+        res.json(movie);
+    });
+});
 
 
 module.exports = router;
