07494e27d364f4cb07487564f8ae00a120159437
mehmet <mehmetkicirti@hotmail.com>
2019-10-30 00:03:45 +0300

    Category schema has been created.

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const CategorySchema = new Schema({
     name:{
         type:String,
-        required:[true, ` Category ({PATH}) was not be null.This is required field.`],
+        required:1,
         minlength:3,
         maxlength:40
     }
 });
 
-module.exports= mongoose.model('category',CategorySchema);
+
+module.exports=mongoose.model('category',CategorySchema);
